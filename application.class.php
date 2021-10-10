9251a376c9a181c3ab0051c225f89a57861e1ed1
lovelylife <life.qm@gmail.com>
2021-10-11 00:26:44 +0800

    merge files

--- application.class.php
+++ application.class.php
@@ -43,7 +43,45 @@ class CLASS_APPLICATION {
   }
 
   function CLASS_APPLICATION($args = array()) { $this->__construct($args); }
- 
+
+  function run($app_root) {
+    global $S_AJAX_MODE;
+    $app_root = str_ireplace('\\', '/', $app_root);
+    
+    if(!file_exists($app_root.'/install.lock')){
+      die("<html><meta http-equiv=\"Content-Type\" content=\"text/html; charset=utf-8\"/> 
+<meta http-equiv=\"Content-Language\" content=\"utf-8\"/> <body>对不起，第一次使用该程序需要安装，请点击<a href=\"install/?app={$appName}\">安装</a></body></html>");
+    }
+
+    // 调用接口参数
+    $args = array();
+    $args['path']     = get_path($app_root.'/');
+    $args['root']     = $app_root;
+    $args['host']     = (is_ssl()?"https://":"http://").$_SERVER["HTTP_HOST"];
+    $args['module']   = $_GET['mod'];
+    $args['action']   = empty($_GET['action'])?"index":$_GET['action'];
+    $args['inajax']   = $S_AJAX_MODE;
+
+  
+    // 检测有效模块，如果不存在使用默认模块处理
+    $valid_module = $args['module'];
+    if(empty($valid_module)) {
+      $valid_module = 'default';
+      $args['module'] = $valid_module;
+    }
+
+    // 创建应用程序目录结构
+    //$paths = $this->checkAppRequired($app_root, $args);
+    //$args = array_merge($args, $paths);
+    //$args['settings'] = &$paths;
+    //if(!class_exists('APPLICATION')) {
+    //  trigger_error('class `APPLICATION` is not found.', E_USER_ERROR);
+    //} 
+    // 创建应用程序实例
+    //$app=new APPLICATION;
+    $this->appMain($args);
+  }  
+
   function initialize( $args ) {
     $this->root_ = $args['root'];
     $this->path_ = $args['path'];
